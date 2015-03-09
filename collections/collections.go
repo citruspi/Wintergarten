9dfa856d8bbc0c5a9da488dc33c3f1d85543133c
Mihir Singh <git.service@mihirsingh.com>
2015-03-08 22:31:13 -0500

    Implemented an endpoint for film collections

---
+++ collections/collections.go
@@ -0,0 +1,95 @@
+package collections
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+type TMDbResponse struct {
+	Dates struct {
+		Maximum string `json:"maximum"`
+		Minimum string `json:"minimum"`
+	} `json:"dates"`
+	Results      []Film `json:"results"`
+	Page         int    `json:"page"`
+	TotalPages   int    `json:"total_pages"`
+	TotalResults int    `json:"total_results"`
+}
+type Film struct {
+	Adult         bool    `json:"adult"`
+	BackdropPath  string  `json:"backdrop_path"`
+	ID            int     `json:"id"`
+	OriginalTitle string  `json:"original_title"`
+	Popularity    float64 `json:"popularity"`
+	PosterPath    string  `json:"poster_path"`
+	ReleaseDate   string  `json:"release_date"`
+	Title         string  `json:"title"`
+	Video         bool    `json:"video"`
+	VoteAverage   float64 `json:"vote_average"`
+	VoteCount     int     `json:"vote_count"`
+}
+
+var (
+	api_key string
+)
+
+func init() {
+	api_key = os.Getenv("TMDB_API_KEY")
+
+	if api_key == "" {
+		log.Fatal("Missing API key")
+	}
+}
+
+func GetFilms(collectionName string) ([]Film, error) {
+	var collection []Film
+
+	response, err := queryTMDb(collectionName)
+
+	if err != nil {
+		return collection, err
+	}
+
+	collection = response.Results
+
+	return collection, nil
+}
+
+func queryTMDb(collectionName string) (TMDbResponse, error) {
+	var url string
+	var response TMDbResponse
+	var buffer bytes.Buffer
+
+	buffer.WriteString("http://api.themoviedb.org/3/movie/")
+	buffer.WriteString(collectionName)
+	buffer.WriteString("?api_key=")
+	buffer.WriteString(api_key)
+
+	url = string(buffer.Bytes())
+
+	resp, err := http.Get(url)
+
+	if err != nil {
+		return response, err
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		return response, err
+	}
+
+	err = json.Unmarshal(body, &response)
+
+	if err != nil {
+		return response, err
+	}
+
+	return response, nil
+}
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"github.com/citruspi/wintergarten/collections"
 	"github.com/citruspi/wintergarten/films"
 	"github.com/gorilla/mux"
 )
@@ -29,10 +30,34 @@ func getFilm(w http.ResponseWriter, r *http.Request) {
 	w.Write(marshalled)
 }
 
+func getFilmCollection(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	collectionName := vars["collection"]
+
+	collection, err := collections.GetFilms(collectionName)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	marshalled, err := json.Marshal(collection)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(marshalled)
+}
+
 func main() {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/film/{id:[0-9]+}/", getFilm)
+	r.HandleFunc("/collection/films/{collection}/", getFilmCollection)
 
 	http.Handle("/", r)
 	http.ListenAndServe(":8000", nil)
